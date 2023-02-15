16be5ecf2d290e121a0118dd16ee68ab8ea124f3
Sanjeewa Kumara Narayana Narayana Mudiyansela <narayans@uoguelph.ca>
2023-02-15 01:32:40 -0500

    set up navbar/header

---
+++ src/components/header/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Cta from './Cta';
+import Navbar from './Navbar'
+
+const Header = () => {
+  return (
+    <>
+      {/* <h1>some tetx</h1> */}
+      <Cta/>
+      <Navbar />
+
+
+    </>
+  )
+}
+
+export default Header;
